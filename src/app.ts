95c5732f6746e9b0c436cd0d2b7be786ff03befc
DUVAN0829 <duvancasgonz29@gmail.com>
2025-01-07 10:10:47 -0500

    crea servidor base en 'app.ts'

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hola mundo')
+
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+
+    console.log(req.url)
+
+    res.write('Saludos')
+    res.end()
+
+})
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080')
+})
